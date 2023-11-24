fe8536adfbd9bf81b55c074f992c490b9dd9c9b9
Dev-Arun7 <123arunblakrishnan@gmail.com>
2023-11-24 22:14:51 +0530

    2.20 main category and products tables are created, crud app also working fine. But need to remove one table from Main_category. and need to keep all products in a single table

--- main_app/models.py
+++ main_app/models.py
@@ -8,18 +8,38 @@ class Main_Category(models.Model):
     objects = models.Manager()
     def __str__(self):
         return str(self.name)
-    
 
-    
-class Category(models.Model):
-    main_category = models.ForeignKey(Main_Category,on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
+
+class Gadget(models.Model):
+    main_category = models.ForeignKey(Main_Category, on_delete=models.CASCADE)
+    brand = models.CharField(max_length=50)
+    model = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    color = models.CharField(max_length=10)
+    display_size = models.IntegerField()
+    camera = models.CharField(max_length=20)
+    storage = models.CharField(max_length=20)
+    ram = models.CharField(max_length=10)
+    network = models.BooleanField()
+    smart = models.BooleanField()
+    battery = models.IntegerField()
+    image = models.ImageField(upload_to='products', default='default_image.jpg')
+
+    stock =  models.IntegerField(default=3)
+    offer = models.PositiveBigIntegerField(default=0,null=True, blank=True)
+    deleted = models.BooleanField(default=False)
+
+
     objects = models.Manager()
+
+    class Meta:
+        abstract = True
+
+class Smartphone(Gadget):
     def __str__(self):
-        return str(self.name) + "--" + str(self.main_category)
-    
+        return f"Smartphone - {self.model}"
 
-
-
-
-
+class Featurephone(Gadget):
+    def __str__(self):
+        return f"Featurephone - {self.model}"
--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from main_app.models import Main_Category,Category
+from main_app.models import Main_Category
 
 # Create your views here.
 def home(request):
     return render(request,'main/home.html')
 
-def all_products(request):
-    return render(request,'product_list/register.html')
 
 def signup(request):
     return render(request,'main/signup.html')
