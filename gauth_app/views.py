b998d6345714f6856263fe562bd5ba6c29297689
Dev-Arun7 <123arunblakrishnan@gmail.com>
2023-12-01 22:44:51 +0530

    v2.75 login page just started

--- gauth_app/views.py
+++ gauth_app/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import  render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
 
 # Create your views here.
+
 def signup(request):
-    return render(request,'main/signup.html')
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect("main_app:home")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render (request=request, template_name="main/register.html", context={"register_form":form})
 
 def login(request):
     return render(request,'main/login.html')
 
+       
