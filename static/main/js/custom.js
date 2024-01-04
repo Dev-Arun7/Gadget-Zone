38b88dd9d8c9b19955ba3030de0181467d955af0
Dev-Arun7 <123arunblakrishnan@gmail.com>
2024-01-04 06:28:33 +0530

    v3.40 quantity change on html is done. now add to cart has to do

--- static/main/js/custom.js
+++ static/main/js/custom.js
@@ -12,4 +12,19 @@ $(document).ready(function(){
         }
 
     });
+
+
+    $('.decrement-btn').click(function (e) {
+        e.preventDefault();
+        var dec_value = $(this).closest('.product_data').find('.qty-input').val();
+        var value = parseInt(dec_value,10);
+        value = isNaN(value) ? 0: value;
+        if(value > 1)
+        {
+            value--;
+            $(this).closest('.product_data').find('.qty-input').val(value);
+        }
+
+    });
+
 });
