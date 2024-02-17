0e4a2041ea91c30f698566f2409c8e72f3e6d38e
Dev-Arun7 <123arunblakrishnan@gmail.com>
2024-02-17 18:36:55 +0530

    v6.50 razorpay is done.

--- static/main/js/checkout.js
+++ static/main/js/checkout.js
@@ -27,12 +27,11 @@ $(document).ready(function(){
                          "image": "https://www.picng.com/upload/letter_g/png_letter_g_50866.png",
                          // "order_id": "order_9A33XWu170gUtm", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
                          "handler": function (responseb){
-                             alert(responseb.razorpay_payment_id);
+                            //  alert(responseb.razorpay_payment_id);
                              data = {
                                 "payment_mode": "razorpay",
                                 "payment_id": responseb.razorpay_payment_id,
-                                csrfmiddlewaretoken: token,
-                                
+                                csrfmiddlewaretoken: token,   
                              }
                              $.ajax({
                                 method:"POST",
@@ -40,15 +39,14 @@ $(document).ready(function(){
                                 data: data,
                                 success: function (responsec){
                                     swal("Razorpay is working...!", responsec.status, "success").then((value) => {
-                                        window.location.href = '/home'
+                                        // window.location.href = '/home' 
+                                       
                                     });
-            
+                                    
                                 }
 
-                             });
-                                                                            
-
-         
+                            });
+                            $("#order-form").submit();
                          },
                          "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information, especially their phone number
                              "name": "Gadgetzone", //your customer's name
@@ -59,6 +57,7 @@ $(document).ready(function(){
                              "color": "#3399cc"
                          }
                      };
+
                     var rzp1 = new Razorpay(options);
                     rzp1.on('payment.failed', function (response){
                             alert(response.error.code);
@@ -70,6 +69,7 @@ $(document).ready(function(){
                             alert(response.error.metadata.payment_id);
                     });
                     rzp1.open();
+                   
                 }
             });
            
@@ -79,57 +79,3 @@ $(document).ready(function(){
 
 
 
-
-
-
-// Nshir code
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     var payButton = document.querySelector('.payWithRazorpay');
-
-//     payButton.addEventListener('click', function(event) {
-//         event.preventDefault();
-
-//         var selects = document.querySelector("[name='addressId']").value;
-
-//         if (selects === "") {
-//             swal("Alert", "Address field is needed", "error");
-//             return false;
-//         } else {
-//             fetch('/razorpay')
-//                 .then(function(response) {
-//                     return response.json();
-//                 })
-//                 .then(function(data) {
-//                     var options = {
-//                         key: 'rzp_test_oR7x1WyMRe9zxr',
-//                         amount: response.total_offer_price * 100,
-//                         currency: 'INR',
-//                         name: 'techzone',
-//                         description: 'Thank you for Placing an order ',
-//                         image: 'https://static-00.iconduck.com/assets.00/bill-payment-icon-2048x2048-vpew78n5.png',
-//                         handler: function(responseb) {
-//                             window.location.href = '/razorpay/' + selects;
-//                         },
-//                         prefill: {
-//                             name: '',
-//                             email: '',
-//                             contact: ''
-//                         },
-//                         notes: {
-//                             address: 'Razorpay Corporate Office'
-//                         },
-//                         theme: {
-//                             color: '#D19C97'
-//                         }
-//                     };
-
-//                     var rzp1 = new Razorpay(options);
-//                     rzp1.open();
-//                 })
-//                 .catch(function(error) {
-//                     console.error('Error:', error);
-//                 });
-//         }
-//     });
-// });
